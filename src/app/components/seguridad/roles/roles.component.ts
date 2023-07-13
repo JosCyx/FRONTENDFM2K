e6672b0b403d20da387728b408fd8245ce2479a6
JosCyx <joseguillermojm.jm@gmail.com>
2023-07-13 13:04:58 -0500

    rol termiado 100%

--- src/app/components/seguridad/roles/roles.component.ts
+++ src/app/components/seguridad/roles/roles.component.ts
@@ -13,7 +13,6 @@ import { CommunicationApiService } from 'src/app/services/communication-api.serv
 export class RolesComponent implements OnInit {
   //variables para guardar los datos del rol nuevo
   nombre: string = '';
-  aplicacion: string = '';//hay que borrar
   idAplicacion: number = 0;
   estado: string = '';
 
@@ -22,10 +21,8 @@ export class RolesComponent implements OnInit {
   //VARIABLE USADA PARA CONTROLAR FUNCIONES DE LA PAGINA
   changeview: string = 'consulta';
   edicion: boolean = false;
-  roCodigo: number = 0;
+  roCodigo: number = 0;//almacena el id del rol que se seleccione
   mensajeExito: string = '';
-
-  activeStatus: boolean = false;
 
   //listas
   rolList$!: Observable<any[]>;
@@ -75,7 +72,7 @@ export class RolesComponent implements OnInit {
     //vacía las variables antes de cambiar de vista para que no muestren datos
     this.codigo = '';
     this.nombre = '';
-    this.aplicacion = '';
+    this.idAplicacion = 0;
     this.estado = '';
 
     this.changeview = view;
@@ -99,6 +96,7 @@ export class RolesComponent implements OnInit {
     this.changeview = 'consulta';
   }
 
+  //recibe el id del rol y lo guarda en una variable local y trae los datos de dicho rol desde la API para cargarlos en las variables locales
   editarRol(index: number): void {
     // Guardar el valor de la posición del elemento a editar
     this.roCodigo = index;
@@ -112,7 +110,6 @@ export class RolesComponent implements OnInit {
         this.idAplicacion = response.roAplicacion;
         this.estado = response.roEstado;
   
-        
       },
       error => {
         // Manejar cualquier error que ocurra durante la llamada a la API aquí
@@ -122,97 +119,43 @@ export class RolesComponent implements OnInit {
     // Cambiar la variable de vista para mostrar la pantalla de edición
     this.changeview = 'editar';
   }
-  
 
-  // editarRol(rol: Rol, index: number): void {
-  //   //guarda el valor de la posicion del elemento a editar
-  //   this.roCodigo = index;
-
-  //   // Asignar los valores del objeto rol a las propiedades existentes
-  //   this.codigo = rol.codigo;
-  //   this.nombre = rol.nombre;
-  //   this.aplicacion = rol.aplicacion;
-  //   this.estado = rol.estado;
-
-  //   //cambia la variable de vista para mostrar la pantalla de edicion
-  //   this.changeview = 'editar';
-  // }
-  
-
-  //LISTAS DONDE SE GUARDAN LOS ROLES
-  //listaRoles: Rol[] = [];
-
-  //elimina el elemento en la posicion indicada por "index" de la lista de roles 
-  eliminarRol(index: number): void {
-    //this.listaRoles.splice(index, 1);
-  }
-
-
+    //guarda los datos almacenados en las variables locales en un data y los envia a la api con el metodo updateRols
   guardarEdicion(): void {
     const data = {
       roEmpresa: 1, // Ajusta el valor según tus requisitos
+      roCodigo:this.roCodigo,
       roNombre: this.nombre,
       roEstado: this.estado,
       roAplicacion: this.idAplicacion // Ajusta el valor según tus requisitos
     };
   
-    // Obtener el código del rol a actualizar
-    this.rolList$.subscribe((roles: Rol[]) => {
-      const codigoRol = roles[this.roCodigo].codigo;
-  
-      // Llamar al método updateRols() del servicio para enviar los datos actualizados a la API
-      this.service.updateRols(codigoRol, data).subscribe(
-        response => {
-          // Manejar la respuesta de la API aquí si es necesario
-          console.log('Rol actualizado exitosamente:', response);
-  
-          // Actualizar la lista de roles después de la edición
-          this.rolList$ = this.service.getRolsList();
-  
-          // Restablecer las variables locales a sus valores iniciales
-          this.codigo = '';
-          this.nombre = '';
-          this.aplicacion = '';
-          this.estado = '';
-  
-          // Cambiar la vista de nuevo a la lista de roles
-          this.changeview = 'consulta';
-        },
-        error => {
-          // Manejar cualquier error que ocurra durante la llamada a la API aquí
-          console.error('Error al actualizar el rol:', error);
-        }
-      );
-    });
+    // Llamar al método updateRols() del servicio para enviar los datos actualizados a la API
+    this.service.updateRols(this.roCodigo, data).subscribe(
+      response => {
+        // Manejar la respuesta de la API aquí si es necesario
+        console.log('Rol actualizado exitosamente:', response);
+      },
+      error => {
+        // Manejar cualquier error que ocurra durante la llamada a la API aquí
+        console.error('Error al actualizar el rol:', error);
+      }
+    );
+    //muestra mensaje de exito y redirige a la otra vista luego de 1 segundo
+    this.mensajeExito = 'Rol editado exitosamente.';
+    setTimeout(() => {
+      // Restablecer las variables locales a sus valores iniciales
+      this.codigo = '';
+      this.nombre = '';
+      this.idAplicacion = 0;
+      this.estado = '';
+      this.mensajeExito = '';
+      this.changeview = 'consulta';
+      }, 1000);
   }
   
-  
-  
-  
-
-
-  // guardarEdicion(): void {
-
-  //   // Crear un nuevo objeto de tipo Rol con los valores de las propiedades existentes
-  //   const rolEditado: Rol = {
-  //     codigo: this.codigo,
-  //     nombre: this.nombre,
-  //     aplicacion: this.aplicacion,
-  //     estado: this.estado
-  //   };
-
-  //   // Reemplazar el elemento en la posición index de la listaRoles con el rol editado
-  //   this.listaRoles.splice(this.indice, 1, rolEditado);
-
-  //   // Restablecer los valores de las propiedades
-  //   this.codigo = '';
-  //   this.nombre = '';
-  //   this.aplicacion = '';
-  //   this.estado = '';
-
-  //   //cambia la variable de vista para mostrar la lista de roles
-  //   this.edicion = false;
-  //   this.changeview = 'consulta';
-  // }
-
+  //elimina el elemento en la posicion indicada por "index" de la lista de roles 
+  eliminarRol(index: number): void {
+    //this.listaRoles.splice(index, 1);
+  }
 }
