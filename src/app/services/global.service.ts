aab7e749443497c70da758ddff307a593eba8015
JosCyx <joseguillermojm.jm@gmail.com>
2023-07-14 12:03:46 -0500

    a

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -5,8 +5,6 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalService {
   isLogin: boolean = false;
-  admin: boolean = false;
-  sol: boolean = false;
   appSelected: boolean = false;
 
   constructor() { }
