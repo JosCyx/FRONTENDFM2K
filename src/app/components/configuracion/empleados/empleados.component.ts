899a76b85abe6770b4b63a16cad28ca5d9880465
JosCyx <joseguillermojm.jm@gmail.com>
2023-07-20 15:01:40 -0500

    empleado final

--- src/app/components/configuracion/empleados/empleados.component.ts
+++ src/app/components/configuracion/empleados/empleados.component.ts
@@ -31,11 +31,12 @@ export class EmpleadosComponent implements OnInit {
   changeview: string = 'consulta';
   edicion: boolean = false;
   mensajeExito: string = '';
-  currentPage: number = 1 ;
+  currentPage: number = 1;
   showOther: boolean = false;
   checkSexo: string = '';
   buscarOpcion: string = 'id';
   terminoBusqueda: string = '';
+  lastIdnomina: number = 0;
   empleados: any[] = [];//lista para almacenar los empleados filtrados
 
   //listas que almacenan el contenido de las tablas
@@ -43,29 +44,43 @@ export class EmpleadosComponent implements OnInit {
   areaList$!: Observable<any[]>;
   dptoList$!: Observable<any[]>;
 
-  
+
   constructor(private service: CommunicationApiService) { }
 
   ngOnInit() {
+    //carga los datos de la tabla empleados en un observable
     this.empleadoList$ = this.service.getEmpleadosList();
 
+    //pasa los datos del observable de empleados a una lista
     this.empleadoList$.subscribe((data) => {
       this.empleados = data;
     });
 
-    
-
+    //carga los datos de la tabla area en un observable
     this.areaList$ = this.service.getAreaList();
 
-    //devuelve el contenido de la tabla de departamentos ordenada alfabeticamente
+    //carga el contenido de la tabla de departamentos en un observable ordenandolos en orden alfabético
     this.dptoList$ = this.service.getDepartamentos().pipe(
       map(departamentos => departamentos.sort((a, b) => a.depDescp.localeCompare(b.depDescp)))
     );
+
+    /*recorre la lista de empleados y guarda el ultimo id de nomina que sea menor a 10000 luego de un tiempo 
+    para asegurarse que los datos ya esten cargados en la lista*/
+    setTimeout(() => {
+      for (const empleado of this.empleados) {
+        /* Si el IdNomina del empleado es menor a 10000, actualizamos la variable lastIdnomina,
+        tomo el valor de 10000 pues de ahi en adelante hay IDs reservados*/
+        if (empleado.empleadoIdNomina < 10000) {
+          this.lastIdnomina = empleado.empleadoIdNomina;
+        }
+      }
+    }, 100);
   }
 
-  buscar() {
+  //busca en la lista empleados segun el termino de busqueda proporcionado por la variable terminoBusqueda
+  search() {
     const term = this.terminoBusqueda.trim().toLowerCase();
-  
+
     if (term === '') {
       // Si el término de búsqueda está vacío, mostrar todos los empleados
       this.empleadoList$.subscribe((data) => {
@@ -89,15 +104,14 @@ export class EmpleadosComponent implements OnInit {
       });
     }
   }
-  
 
   //incrementa el valor d la variable que controla la pagina actual que se muestra
-  nextPage():void {
+  nextPage(): void {
     this.currentPage++;
   }
 
   //decrementa el valor de la variable que controla la pagina actual que se muestra
-  prevPage():void {
+  prevPage(): void {
     if (this.currentPage > 1) {
       this.currentPage--; // Disminuir currentPage en uno si no está en la primera página
     }
@@ -114,16 +128,17 @@ export class EmpleadosComponent implements OnInit {
     this.edicion = false;
   }
 
-  checkOtro(){
-    if( this.checkSexo === 'M'){
+  //controla el input de texto para ingresar otro sexo que no sea F o M
+  checkOtro() {
+    if (this.checkSexo === 'M') {
       this.showOther = false;
       this.empSexo = this.checkSexo;
       console.log(this.empSexo);
-    }else if(this.checkSexo === 'F'){
+    } else if (this.checkSexo === 'F') {
       this.showOther = false;
       this.empSexo = this.checkSexo;
       console.log(this.empSexo);
-    }else if (this.checkSexo === 'otro'){
+    } else if (this.checkSexo === 'otro') {
       this.empSexo = '';
       this.checkSexo = 'otro'
       this.showOther = true;
@@ -131,7 +146,7 @@ export class EmpleadosComponent implements OnInit {
   }
 
   //limpia todas las variables locales que guardan los datos del empleado
-  clear():void{
+  clear(): void {
     this.empNombres = '';
     this.empApellidos = '';
     this.empCorreo = '';
@@ -147,7 +162,7 @@ export class EmpleadosComponent implements OnInit {
   }
 
   //limpia la variable que guarda el temrino a buscar y ejecuta ngOnInit
-  clearSearch():void{
+  clearSearch(): void {
     this.terminoBusqueda = '';
     this.ngOnInit();
   }
@@ -158,7 +173,7 @@ export class EmpleadosComponent implements OnInit {
     this.changeview = 'consulta';
   }
 
-  agregarEmpleado(){
+  agregarEmpleado() {
     const data = {
       empleadoCompania: 1,
       empleadoIdNomina: this.empIdNomina,
@@ -178,6 +193,12 @@ export class EmpleadosComponent implements OnInit {
       response => {
         // Manejar la respuesta de la API aquí si es necesario
         console.log('Empleado agregado exitosamente:', response);
+        this.mensajeExito = 'Empleado registrado exitosamente.';
+        setTimeout(() => {
+          this.mensajeExito = '';
+          this.changeview = 'consulta';
+        }, 1500);
+        this.ngOnInit();
       },
       error => {
         // Manejar cualquier error que ocurra durante la llamada a la API aquí
@@ -185,11 +206,7 @@ export class EmpleadosComponent implements OnInit {
       }
     );
 
-    this.mensajeExito = 'Empleado registrado exitosamente.';
-    setTimeout(() => {
-      this.mensajeExito = '';
-      this.changeview = 'consulta';
-      }, 1000);
+
   }
 
   editarEmpleado(id: number) {
@@ -221,7 +238,7 @@ export class EmpleadosComponent implements OnInit {
   }
 
   guardarEdicion(): void {
-    //es necesario enviar todos los campos cuando se realiza u put (edicion), de lo contrario retornara errores
+    //es necesario enviar todos los campos cuando se realiza un put (edicion), de lo contrario retornara errores
     const data = {
       empleadoId: this.empId,
       empleadoIdNomina: this.empIdNomina,
@@ -238,26 +255,28 @@ export class EmpleadosComponent implements OnInit {
       empleadoEstado: this.empEstado
     };
 
-    // Llamar al método updateRols() del servicio para enviar los datos actualizados a la API
+    // Llamar al método updateEmpleado() del servicio para enviar los datos actualizados a la API
     this.service.updateEmpelado(this.empId, data).subscribe(
       response => {
         // Manejar la respuesta de la API aquí si es necesario
         console.log('Empleado actualizado exitosamente:', response);
+
+        //muestra mensaje de exito y redirige a la otra vista luego de 1.5 segundo
+        this.mensajeExito = 'Empleado editado exitosamente.';
+        setTimeout(() => {
+          // Restablecer las variables locales a sus valores iniciales
+          this.clear();
+          this.changeview = 'consulta';
+        }, 1500);
+
+        this.ngOnInit();
       },
       error => {
         // Manejar cualquier error que ocurra durante la llamada a la API aquí
         console.error('Error:', error);
       }
     );
-    //muestra mensaje de exito y redirige a la otra vista luego de 1 segundo
-    this.mensajeExito = 'Empleado editado exitosamente.';
-    setTimeout(() => {
-      // Restablecer las variables locales a sus valores iniciales
-      this.clear();
 
-      this.changeview = 'consulta';
-    }, 1000);
   }
-
 }
 
