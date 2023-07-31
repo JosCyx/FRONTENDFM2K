f983d9f48403143dc7438dcb9f67468d07128d28
JosCyx <joseguillermojm.jm@gmail.com>
2023-07-31 13:33:56 -0500

    formulario crear solicitud

--- src/app/components/procesos/solicoti/solicoti.component.ts
+++ src/app/components/procesos/solicoti/solicoti.component.ts
@@ -7,30 +7,14 @@ import { Solicoti } from 'src/app/models/procesos/solicoti';
   styleUrls: ['./solicoti.component.css']
 })
 export class SolicotiComponent {
-  changeview: string = 'consultar';
-  indice: number = 0;
-  listaCoti: Solicoti[] = [];
+  empleado: string = '';
   fecha: Date = new Date;
-  sector: string = '';
-  asunto:string = '';
-  nrosoli: string = '';
-  solicitadopor: string = '';
 
+  changeview: string = 'crear';
 
   
   changeView(view: string): void {
     this.changeview = view;
   }
-  
-  eliminarSolicoti(index:number):void{
-    this.listaCoti.splice(index,1);
-  }
-  editarSolicoti(solicoti: Solicoti, index: number): void {
-    this.indice = index;
-    this.fecha = solicoti.fecha;
-    this.sector = solicoti.sector;
-    this.asunto = solicoti.asunto;
-    this.nrosoli = solicoti.nrosoli;
-    
-}
+
 }
