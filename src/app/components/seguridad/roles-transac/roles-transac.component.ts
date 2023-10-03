c7ddb0fc1d56c6b49b60a5ca928e99ac3a5ef612
JosCyx <joseguillermojm.jm@gmail.com>
2023-10-03 16:34:26 -0500

    roles-transacciones

--- src/app/components/seguridad/roles-transac/roles-transac.component.ts
+++ src/app/components/seguridad/roles-transac/roles-transac.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RolesService } from 'src/app/services/comunicationAPI/seguridad/roles.service';
+import { TransaccionesService } from 'src/app/services/comunicationAPI/seguridad/transacciones.service';
 
 @Component({
   selector: 'app-roles-transac',
   templateUrl: './roles-transac.component.html',
   styleUrls: ['./roles-transac.component.css']
 })
-export class RolesTransacComponent {
+export class RolesTransacComponent implements OnInit{
+
+  transacList!: any[];
+  rolList!: any[];
+  rolAsign: number =0;
+
+  changeview:string = 'asignar';
+
+  constructor(private transacService: TransaccionesService,
+              private rolService: RolesService) { }
+
+  ngOnInit(): void {
+    this.transacService.getTransaccionesList().subscribe(
+      response => {
+        this.transacList = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
+    this.rolService.getRolsList().subscribe(
+      response => {
+        this.rolList = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  changeView(view: string){
+    this.changeview = view;
+  }
+
+  
+
 
 }
