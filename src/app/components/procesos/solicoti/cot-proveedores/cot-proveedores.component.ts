329bbbc26a024b182b1a20da985bc21faca293c0
JosCyx <joseguillermojm.jm@gmail.com>
2023-11-01 15:19:14 -0500

    correccion de proveedores de cotizacion

--- src/app/components/procesos/solicoti/cot-proveedores/cot-proveedores.component.ts
+++ src/app/components/procesos/solicoti/cot-proveedores/cot-proveedores.component.ts
@@ -1079,6 +1079,7 @@ RecorrerPro(){
         //console.log('Eliminado con exito!');
         this.showmsj=true;
         this.msjExito="Solicitud Cancelada con exito";
+        this.getProvCotizacion();
         setTimeout(() => {
           this.showmsj=false;
           this.msjExito="";
@@ -1087,7 +1088,7 @@ RecorrerPro(){
       error: error => {
         console.error('Error al Eliminar!', error);
         this.showmsjerror=true;
-        this.msjError="Error al cancelar la solicitud";
+        this.msjError="Error, ya se han eliminado los proveedores.";
         setTimeout(() => {
           this.showmsjerror=false;
           this.msjError="";
